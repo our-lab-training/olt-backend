4442904e717e887da0fae72adadb9ae57333f2f4
fraser <mrfrase3@gmail.com>
2018-10-31 15:04:11 +0800

    remove module config

--- src/services/index.js
+++ src/services/index.js
@@ -5,7 +5,6 @@ const questions = require('./questions/questions.service.js');
 const attempts = require('./attempts/attempts.service.js');
 const trainings = require('./trainings/trainings.service.js');
 const perms = require('./perms/perms.service.js');
-const moduleConfigs = require('./module-configs/module-configs.service.js');
 const inductions = require('./inductions/inductions.service.js');
 const roles = require('./roles/roles.service.js');
 // eslint-disable-next-line no-unused-vars
@@ -17,7 +16,6 @@ module.exports = function (app) {
   app.configure(attempts);
   app.configure(trainings);
   app.configure(perms);
-  app.configure(moduleConfigs);
   app.configure(inductions);
   app.configure(roles);
 };
