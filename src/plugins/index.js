d1c09ee576a0999723cef9475f647348491be592
fraser <mrfrase3@gmail.com>
2018-11-05 11:47:14 +0800

    no longer require plugin.json

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -7,10 +7,11 @@ module.exports = (app) => {
     let config = null;
     try {
       if(!fs.statSync(`${__dirname}/${dir}`).isDirectory()) continue;
-      config = require(`${__dirname}/${dir}/plugin.json`);
+      if(!fs.statSync(`${__dirname}/${dir}/api`).isDirectory()) continue;
+      /* config = require(`${__dirname}/${dir}/plugin.json`);
       config.ref = config.ref || dir;
-      app.plugins[config.ref] = config;
-      app.configure(require(`${__dirname}/${dir}/api/index`));
+      app.plugins[config.ref] = config; */
+      app.configure(require(`${__dirname}/${dir}/api/`));
     } catch(err) {
       console.error(err); // eslint-disable-line no-console
       if(config && config.ref && app.plugins[config.ref]) delete app.plugins[config.ref];
