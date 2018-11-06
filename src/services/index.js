591c2072c1ec49a794bcb0fe1d56b21e73b6e42e
fraser <mrfrase3@gmail.com>
2018-11-06 13:22:56 +0800

    move services into sub-modules

--- src/services/index.js
+++ src/services/index.js
@@ -1,21 +1,11 @@
 const users = require('./users/users.service.js');
 const groups = require('./groups/groups.service.js');
-const quizzes = require('./quizzes/quizzes.service.js');
-const questions = require('./questions/questions.service.js');
-const attempts = require('./attempts/attempts.service.js');
-const trainings = require('./trainings/trainings.service.js');
 const perms = require('./perms/perms.service.js');
-const inductions = require('./inductions/inductions.service.js');
 const roles = require('./roles/roles.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(groups);
-  app.configure(quizzes);
-  app.configure(questions);
-  app.configure(attempts);
-  app.configure(trainings);
   app.configure(perms);
-  app.configure(inductions);
   app.configure(roles);
 };
