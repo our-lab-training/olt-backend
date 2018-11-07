ede945c32586443ca095aac08b0787b92be83807
fraser <mrfrase3@gmail.com>
2018-11-07 11:49:50 +0800

    load mongo uri from config

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = function (app) {
-  mongoose.connect(app.get('mongodb'), { useNewUrlParser: true });
+  mongoose.connect(process.env.MONGO_URI || app.get('mongodb'), { useNewUrlParser: true });
   mongoose.Promise = global.Promise;
 
   app.set('mongooseClient', mongoose);
