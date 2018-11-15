47f8d32a71cc90b24ec6cfa5241ef06b8b453e7d
mrfrase3 <mrfrase3@gmail.com>
2018-11-15 08:25:26 +0000

    only add everyone if group is global

--- src/hooks/populate-globals.js
+++ src/hooks/populate-globals.js
@@ -5,19 +5,19 @@
 module.exports = function (options = {}) {
   return async context => {
 
-    const {app, serviceName, params} = context;
+    const {app, serviceName, params, result} = context;
     const permsService = app.service('perms');
     
-    if(serviceName === 'groups'){
+    if(serviceName === 'groups' && result.type === 'global'){
       const users = await app.service('users').find({query: {$select: ['_id']}, paginate: false});
-      const groupId = context.result._id;
+      const groupId = result._id;
       users.forEach(user => {
         permsService.create({grantee: user._id, perm: [groupId, 'enrolled'], type: 'users'}, params);
       });
     }
     if(serviceName === 'users'){
       const groups = await app.service('groups').find({query: {type: 'global', $select: ['_id']}, paginate: false});
-      const userId = context.result._id;
+      const userId = result._id;
       groups.forEach(group => {
         permsService.create({grantee: userId, perm: [group._id, 'enrolled'], type: 'users'}, params);
       });
