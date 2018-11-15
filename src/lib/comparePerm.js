a41fb9b8dfb403015f8faad0b6e34d4994c41c2c
mrfrase3 <mrfrase3@gmail.com>
2018-11-15 12:15:36 +0000

    allow for partial wildcards in perm nodes close #12

--- src/lib/comparePerm.js
+++ src/lib/comparePerm.js
@@ -1,9 +1,13 @@
+const node2reg = node => new RegExp(node.replace(/\//g, '\\/').replace(/\*/g, '.*'));
+
 module.exports = async (perm, p, exact = false) => {
   if(typeof perm === 'string') perm = perm.split('.');
   if(exact && p.length !== perm.length) return false;
   for(let i = 0; i < Math.min(p.length, perm.length); i++){
     if (!exact && p[i] === '*') return true;
-    if ((exact || perm[i] !== '*') && perm[i] !== p[i]) return false;
+    if (p[i] === '*' || p[i].indexOf('*') === -1 || !node2reg(p[i]).test(perm[i])) {
+      if ((exact || perm[i] !== '*') && perm[i] !== p[i]) return false;
+    }
   }
   return true;
 };
