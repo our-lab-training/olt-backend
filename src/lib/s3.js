8a9d7c32f5b7505b107921e0768aac4313905aa7
Fraser Bullock <mrfrase3@gmail.com>
2018-12-07 02:28:48 +0800

    update to s3 v4 sigs, because it's the future!

--- src/lib/s3.js
+++ src/lib/s3.js
@@ -1,14 +1,19 @@
 const AWS = require('aws-sdk');
 
 module.exports = (app) => {
+  AWS.config.update({
+    signatureVersion: 'v4',
+  });
   app.buckets = {
     public: new AWS.S3({
       apiVersion: '2006-03-01',
-      params: {Bucket: process.env.AWS_BUCKET_PUBLIC}
+      params: {Bucket: process.env.AWS_BUCKET_PUBLIC},
+      signatureVersion: 'v4',
     }),
     private: new AWS.S3({
       apiVersion: '2006-03-01',
-      params: {Bucket: process.env.AWS_BUCKET_PRIVATE}
+      params: {Bucket: process.env.AWS_BUCKET_PRIVATE},
+      signatureVersion: 'v4',
     }),
   };
 };
