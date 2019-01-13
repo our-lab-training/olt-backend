68b4a831ca1b639295bd38571ef746315e863cb9
fraser <mrfrase3@gmail.com>
2019-01-14 01:59:10 +0800

    typedObjectId doesn't work on updates...

--- src/types/typedObjectId.type.js
+++ src/types/typedObjectId.type.js
@@ -4,9 +4,17 @@ const _ = require('lodash');
 module.exports = (field, app, opts = { required: true } ) => ({
   type: Schema.Types.ObjectId,
   validate: async function(v){
-    if(!v) throw new Error(`${_.get(this, field)} reference is required.`);
-    const row = await app.service(_.get(this, field)).get(v);
-    if(!row) throw new Error(`${_.get(this, field)} reference does not exist.`);
+    try {
+      const service = _.get(this, field);
+      if(!service) throw new Error(`service name not specified. \n${Object.keys(this)}`);
+      if(!v) throw new Error(`${service} reference is required.`);
+      const row = await app.service(service).get(v);
+      if(!row) throw new Error(`${service} reference does not exist.`);
+    } catch(err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+      throw err;
+    }
   },
   ...opts,
 });
