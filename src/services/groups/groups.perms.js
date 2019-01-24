91f48cade98d0eb34daafb0d62719d9b7b07786d
fraser <mrfrase3@gmail.com>
2019-01-24 21:08:01 +0800

    allow pheme number to be passed instead on an ID when enrolling

--- src/services/groups/groups.perms.js
+++ src/services/groups/groups.perms.js
@@ -3,7 +3,7 @@ const checkPerm = require('../../lib/checkPerm');
 module.exports = (app) => {
   app.perms.addPermListener('*.enrolled', async (context, perm) => {
     const { params, data, existing } = context;
-    const { grantee } = existing || data;
+    let { grantee, type } = existing || data;
     const groupId = perm[0];
     const hasPerm = await (async () => {
       if(checkPerm(`${groupId}.users.write`, params.user)) return true;
@@ -12,6 +12,21 @@ module.exports = (app) => {
       return ['public', 'global'].indexOf(group.type) !== -1;
     })();
     if (!hasPerm) return false;
+    if (type === 'roles') return hasPerm;
+    if (/^\d{8}$/.test(grantee)) {
+      let user = (await app.service('users').find({ query: { username: grantee }, paginate: false}))[0];
+      if (!user) user = await app.service('users').create({
+        username: grantee,
+        email: `${grantee}@pending.uwa.edu.au`,
+        profile: {
+          displayname: 'Details Pending First Login',
+          firstname: 'Details Pending',
+          lastname: 'First Login',
+        },
+      });
+      data.grantee = user._id;
+      grantee = user._id;
+    }
     const roles = await app.service('roles').find({query: {groupId, addOnJoin: true}, paginate: false});
     await Promise.all(roles.map(async (role) => {
       await app.service('perms').create({
