d2bfe955b9f1da36e15d578bfb22c71052c8ae26
fraser <mrfrase3@gmail.com>
2019-02-05 16:04:28 +0800

    fix 4am spelling error

--- src/hooks/restrict-method.js
+++ src/hooks/restrict-method.js
@@ -6,14 +6,14 @@ const errors = require('@feathersjs/errors');
 const checkPerm = require('../lib/checkPerm');
 
 // eslint-disable-next-line no-unused-vars
-module.exports = function (permimmsions='*', opts = {}) {
-  if(!Array.isArray(permimmsions)) permimmsions = [permimmsions];
+module.exports = function (permissions='*', opts = {}) {
+  if(!Array.isArray(permissions)) permissions = [permissions];
   return async context => {
     const {params} = context;
     if(!params.provider) return context;
     if(!params.user) throw new errors.NotAuthenticated('You must be logged in.');
 
-    for(let perm of permimmsions){
+    for(let perm of permissions){
       const autofills = perm.match(/{[\w.[\]]+}/g) || [];
       for(let autofill of autofills) perm = perm.replace(autofill, _.get(context, autofill));
       const hasPerm = checkPerm(perm, params.user);
