06491d18c88a61dc31f02c783724b9cbb7d7c6a8
mrfrase3 <mrfrase3@gmail.com>
2019-05-10 22:04:15 +0800

    increase default desc length

--- src/types/desc.type.js
+++ src/types/desc.type.js
@@ -1,5 +1,5 @@
 
-module.exports = (required=[false, 'Description is required.'], length=1024, opts) => ({
+module.exports = (required=[false, 'Description is required.'], length=16384, opts) => ({
   type: String,
   maxlength: [length, `The description cannot exceed ${length} characters`],
   trim: true,
