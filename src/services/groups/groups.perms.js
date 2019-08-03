c0d8adc92e603d4e71ecfe7293e967ce345b6c24
mrfrase3 <mrfrase3@gmail.com>
2019-08-03 11:00:43 +0800

    load in users from pheme

--- src/services/groups/groups.perms.js
+++ src/services/groups/groups.perms.js
@@ -1,4 +1,5 @@
 const checkPerm = require('../../lib/checkPerm');
+const request = require('request-promise');
 
 module.exports = (app) => {
   app.perms.addPermListener('*.enrolled', async (context, perm) => {
@@ -13,17 +14,30 @@ module.exports = (app) => {
     })();
     if (!hasPerm) return false;
     if (type === 'roles') return hasPerm;
-    if (/^\d{8}$/.test(grantee)) {
+    if (/^\d{8}$/.test(grantee) && data.authUser && data.authPass) {
       let user = (await app.service('users').find({ query: { username: grantee }, paginate: false}))[0];
-      if (!user) user = await app.service('users').create({
-        username: grantee,
-        email: `${grantee}@pending.uwa.edu.au`,
-        profile: {
-          displayname: 'Details Pending First Login',
-          firstname: 'Details Pending',
-          lastname: 'First Login',
-        },
-      });
+      if (!user) {
+        const res = await request({
+          method: 'POST',
+          uri: `https://auth.systemhealthlab.com/api/users/${grantee}`,
+          body: {
+            user: data.authUser,
+            pass: data.authPass,
+            token: process.env.AUTH_TOKEN,
+          },
+          json: true,
+        });
+        if (!res.success) return false;
+        user = await app.service('users').create({
+          username: res.user.username,
+          email: res.user.email,
+          profile: {
+            firstname: res.user.firstname,
+            lastname: res.user.lastname,
+            displayname: res.user.firstname,
+          }
+        });
+      }
       data.grantee = user._id;
       grantee = user._id;
     }
