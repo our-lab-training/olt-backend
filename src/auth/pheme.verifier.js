a6a8e80e91f6c9e30b7d215e4c80d97b69ca3815
frinzekt <frinzekt@gmail.com>
2020-09-18 15:27:14 +0800

    custom pheme authentication with JWT in Feathers updated

--- src/auth/pheme.verifier.js
+++ src/auth/pheme.verifier.js
@@ -1,57 +1,62 @@
 const request = require('request-promise-native');
+const { NotAuthenticated } = require('@feathersjs/errors');
+const { LocalStrategy } = require('@feathersjs/authentication-local');
 
 if (process.env.NODE_ENV !== 'production') {
   console.warn('Warning: not in production mode, enabling demo user'); // eslint-disable-line
 }
 
-module.exports = (app) => async (req, done) => {
-  try {
-    const { username, password } = req.query;
-    const isDemoUser = process.env.NODE_ENV !== 'production' && username === '12345678' && password === 'demo';
-    let body = null;
-    if (isDemoUser) {
-      body = { success: true, user: { username, email: '12345678@example.uwa.edu.au', firstname: 'Jo', lastname: 'Blogs' } };
-    } else {
-      try {
-        body = await request({
-          method: 'POST',
-          uri: 'https://auth.systemhealthlab.com/api/login',
-          body: {
-            user: username,
-            pass: password,
-            token: process.env.AUTH_TOKEN,
-          },
-          json: true,
-        });
-      } catch (err) {
-        if (err.statusCode >= 400 && err.statusCode < 500) throw new Error(err.error.message);
-        console.error(err); // eslint-disable-line
-        throw new Error('Unknown login issue occured, please contact an administrator.');
-      }
-    }
-    if (!body.success) throw new Error(body.message);
-    const users = await app.service('users').find({ query: { username: body.user.username }, paginate: false });
-    let user = null;
-    const data = {
-      username: body.user.username,
-      email: body.user.email,
-      profile: {
-        firstname: body.user.firstname,
-        lastname: body.user.lastname,
-        displayname: body.user.firstname,
-      }
-    };
-    if (users.length === 0) {
-      user = await app.service('users').create(data);
+module.exports = (app) => class PhemeStrategy extends LocalStrategy {
+  async authenticate(authentication, params) {
+    try {
+      const { username, password } = authentication;
+      const isDemoUser = process.env.NODE_ENV !== 'production' && username === '12345678' && password === 'demo';
+      let body = null;
       if (isDemoUser) {
-        await app.service('perms').create({ perm: ['*'], type: 'users', grantee: user._id });
+        body = { success: true, user: { username, email: '12345678@example.uwa.edu.au', firstname: 'Jo', lastname: 'Blogs' } };
+      } else {
+        try {
+          body = await request({
+            method: 'POST',
+            uri: 'https://auth.systemhealthlab.com/api/login',
+            body: {
+              user: username,
+              pass: password,
+              token: process.env.AUTH_TOKEN,
+            },
+            json: true,
+          });
+        } catch (err) {
+          if (err.statusCode >= 400 && err.statusCode < 500) throw new Error(err.error.message);
+          console.error(err); // eslint-disable-line
+          throw new Error('Unknown login issue occured, please contact an administrator.');
+        }
       }
-    } else {
-      data.profile.displayname = users[0].profile.displayname || data.profile.displayname;
-      user = await app.service('users').patch(users[0]._id, data);
+      if (!body.success) throw new Error(body.message);
+      const users = await app.service('users').find({ query: { username: body.user.username }, paginate: false });
+      let user = null;
+      const data = {
+        username: body.user.username,
+        email: body.user.email,
+        profile: {
+          firstname: body.user.firstname,
+          lastname: body.user.lastname,
+          displayname: body.user.firstname,
+        }
+      };
+      if (users.length === 0) {
+        user = await app.service('users').create(data);
+        if (isDemoUser) {
+          await app.service('perms').create({ perm: ['*'], type: 'users', grantee: user._id });
+        }
+      } else {
+        data.profile.displayname = users[0].profile.displayname || data.profile.displayname;
+        user = await app.service('users').patch(users[0]._id, data);
+      }
+      return { userId: user._id }
+    } catch (err) {
+      console.log(err)
+      throw new NotAuthenticated(err);
     }
-    return done(null, user, { userId: user._id });
-  } catch (err) {
-    return done(null, false, { message: err.message });
   }
-};
+}
--- src/authentication.js
+++ src/authentication.js
@@ -1,32 +1,38 @@
-const authentication = require('@feathersjs/authentication');
-const jwt = require('@feathersjs/authentication-jwt');
-const local = require('@feathersjs/authentication-local');
+const {
+  AuthenticationService,
+  JWTStrategy,
+} = require('@feathersjs/authentication');
+const { AuthenticationBaseStrategy } = require('@feathersjs/authentication');
 const custom = require('./auth/custom.strategy');
 const phemeVerifierConstructor = require('./auth/pheme.verifier');
+const Strategy = require('passport-custom');
+const { LocalStrategy } = require('@feathersjs/authentication-local');
+
 
 
 module.exports = function (app) {
-  const config = app.get('authentication');
 
-  // Set up authentication with the secret
-  app.configure(authentication(config));
-  app.configure(jwt());
-  // app.configure(local());
+  // SETUP AUTHENTICATION SERVICE
+  const authentication = new AuthenticationService(app);
+  authentication.register('jwt', new JWTStrategy());
 
-  const phemeVerifier = phemeVerifierConstructor(app);
-  app.configure(custom('local', phemeVerifier));
+  const PhemeStrategy = phemeVerifierConstructor(app);
+  authentication.register('local', new PhemeStrategy());
 
-  // The `authentication` service is used to create a JWT.
-  // The before `create` hook registers strategies that can be used
-  // to create a new valid JWT (e.g. local or oauth2)
-  app.service('authentication').hooks({
-    before: {
-      create: [
-        authentication.hooks.authenticate(config.strategies)
-      ],
-      remove: [
-        authentication.hooks.authenticate('jwt')
-      ]
-    }
-  });
+  // app.configure(custom('local', phemeVerifier));
+
+  // // The `authentication` service is used to create a JWT.
+  // // The before `create` hook registers strategies that can be used
+  // // to create a new valid JWT (e.g. local or oauth2)
+  // app.service('authentication').hooks({
+  //   before: {
+  //     create: [
+  //       authentication.hooks.authenticate(config.strategies)
+  //     ],
+  //     remove: [
+  //       authentication.hooks.authenticate('jwt')
+  //     ]
+  //   }
+  // });
+  app.use('/authentication', authentication);
 };
