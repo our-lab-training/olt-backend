62621468e9f78b18cfb112cecdcb0bacf5ce2d7a
frinzekt <frinzekt@gmail.com>
2020-09-25 15:49:50 +0800

    deprecation fixes

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 module.exports = function (app) {
-  mongoose.connect(process.env.MONGO_URI || app.get('mongodb'), { useNewUrlParser: true });
+  mongoose.connect(process.env.MONGO_URI || app.get('mongodb'), { useNewUrlParser: true, useUnifiedTopology: true });
+  mongoose.set('useCreateIndex', true);
   mongoose.Promise = global.Promise;
 
   app.set('mongooseClient', mongoose);
