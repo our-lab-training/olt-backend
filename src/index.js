9f28bdca74ef0453a9ed85fa9983b74e8c783872
frinzekt <frinzekt@gmail.com>
2020-09-26 12:34:03 +0800

    fix dotenv loading

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const logger = require('./logger');
 const app = require('./app');
 const port = process.env.PORT || app.get('port');
@@ -7,7 +8,7 @@ const host = process.env.HOST || app.get('host');
 const server = app.listen(port);
 
 process.on('unhandledRejection', (reason, p) => {
-  if(reason.name === 'MongoNetworkError') return console.error('Error: mongo connection failed.');
+  if (reason.name === 'MongoNetworkError') return console.error('Error: mongo connection failed.');
   console.error('Unhandled Rejection at: Promise ', p, reason);
 });
 
